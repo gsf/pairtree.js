55224be5ed9c0afd2587b27c550603d30eab7b5b
Gabriel Farrell <g@grrawr.com>
2011-07-07 23:05:51 -0400

    Added id assert

--- test/test-id.js
+++ test/test-id.js
@@ -9,17 +9,18 @@ var common = require('./common');
 var equal = common.assert.equal;
 var pairtree = common.pairtree;
 
-common.expected = 10;
+common.expected = 11;
 
 equal(pairtree.id('/ab/cd/'), 'abcd', 'basic 4-char path');
 equal(pairtree.id('/ab/cd/e/'), 'abcde', 'basic 5-char path');
 equal(pairtree.id('ab/cd/e'), 'abcde', 'missing terminal separators');
-equal(pairtree.id('/ab/cd/e/f/gh/'), 'abcde', '1-char shorty ends ppath');
+equal(pairtree.id('/ab/cd/e/f/gh/'), 'abcde', 'shorty ends ppath');
+equal(pairtree.id('/x/ab/cd/'), 'abcd', 'shorty before ppath is skipped');
 equal(pairtree.id('///ab///cd///e///////'), 'abcde', 'lots of bunched separators');
 equal(pairtree.id('  //ab///cd///e///  '), 'abcde', 'whitespace in front and in back');
 equal(pairtree.id('pairtree_root/ab/cd/e/obj'), 'abcde', 'junk before and after path');
 equal(pairtree.id('pairtree_root/ab/c/d/ef'), 'abc', 'junk after one-char component terminates ppath');
 equal(pairtree.id('pairtree_root/a=/c+/e,/obj'), 'a/c:e.', 'junk with weird chars');
 equal(pairtree.id('/home/jak/pairtree_root/ab/cd/e/data/obj'), 'abcde', 'bigger junk before and after path');
-// really? ugh. forget about this one for now
+// really? ugh. that seems contradictory. forget about this one for now
 //equal(pairtree.id('/home/jak/pairtree_root/ab/cd/e/data/obj/pairtree_root/gh/ij'), 'ghij', 'ppath followed by a ppath picks last one');

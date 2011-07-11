7e0666b4b180155b593acc0bc393f4c3e13d3dd2
Gabriel Farrell <gsf747@gmail.com>
2011-07-10 22:59:23 -0400

    Added id assert for utf8 chars

--- test/test-id.js
+++ test/test-id.js
@@ -9,7 +9,7 @@ var common = require('./common');
 var equal = common.assert.equal;
 var pairtree = common.pairtree;
 
-common.expected = 12;
+common.expected = 13;
 
 equal(pairtree.id('/ab/cd/'), 'abcd', 'basic 4-char ppath');
 equal(pairtree.id('/ab/cd/e/'), 'abcde', 'basic 5-char ppath');
@@ -23,6 +23,7 @@ equal(pairtree.id('pairtree_root/ab/c/d/ef'), 'abc', 'junk after one-char compon
 equal(pairtree.id('pairtree_root/a=/c+/e,/obj'), 'a/c:e.', 'junk with weird chars');
 equal(pairtree.id('/home/jak/pairtree_root/ab/cd/e/data/obj'), 'abcde', 'bigger junk before and after path');
 equal(pairtree.id('pairtree_root/wh/at/-t/he/-^/2a/@^/3f/#!/^5/e!/^3/f/obj'), 'what-the-*@?#!^!?', 'junk with reverse of spec example');
+equal(pairtree.id('/5^/e2/^8/2^/ac/^2/0^/d5/^b/d^/d5/^a/5^/d5/^b/6^/d5/^a/4^/d5/^b/e^/d5/^a/b^/d5/^b/9/'), '5€ սենդվիչ');//, 'UTF-8 chars (Armenian for "5-euro sandwich")');
 
 // really? ugh. this seems contradictory. forget about it for now.
 //equal(pairtree.id('/home/jak/pairtree_root/ab/cd/e/data/obj/pairtree_root/gh/ij'), 'ghij', 'ppath followed by a ppath picks last one');
